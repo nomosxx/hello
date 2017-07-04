6b27944a5c431fb368d266943994de44818d8ec6
nomos <nomos.xx@gmail.com>
2017-07-04 11:16:30 +0800

    Fourth Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-import java.util.*;
 public class HelloWorld{
-    System.out.println("Hello,World.");
-    System.out.println("Hello,nomos");
+    public static void main(String[] args){
+        System.out.println("Hello,World.");
+    }
 }
